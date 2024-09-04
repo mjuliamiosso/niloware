f2979af236b6a30ef6298ac63e09b4a97cc76c61
Maria Júlia Miosso <mjuliamiosso@gmail.com>
2024-09-04 16:39:22 -0300

    adding card

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import "./styles/page.scss";
+import ResourceCard from "./components/ResourceCard/ResourceCard";
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 export default function Home() {
   return (
