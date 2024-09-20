1f934532d2658a429dea31877d4155a5e7383eec
Marcos Gomes <mpcgomes2@gmail.com>
2024-09-20 18:10:45 -0300

    add i18n store

--- app/store/store.ts
+++ app/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import themeReducer from './slices/themeSlice';
+import languageReducer from './slices/languageSlice';
 
 export const store = configureStore({
     reducer: {
-        theme: themeReducer
+        theme: themeReducer,
+        language: languageReducer
     }
 });
 
