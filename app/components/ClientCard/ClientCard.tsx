43a5778e6b909e1630a9ab482ed8eb93cf96e197
Maria Júlia Miosso <mjuliamiosso@gmail.com>
2024-09-20 18:48:05 -0300

    add: client card component

---
+++ app/components/ClientCard/ClientCard.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { LiaExternalLinkSquareAltSolid } from "react-icons/lia";
+import styles from './ClientCard.module.scss'
+
+interface ClientCardProps {
+    image: any;
+    link: any;
+}
+
+const ClientCard: React.FC<ClientCardProps> = ({ image, link }) =>{
+  return (
+    <div className={styles['client-card']}>
+        <img src={image} alt="client-image" />
+        <a href={link}>
+            <LiaExternalLinkSquareAltSolid />
+        </a>
+    </div>
+  )
+}
+
+export default ClientCard
