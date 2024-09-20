c6070d2c985ea997bd0cf405d223c4264374c324
Marcos Gomes <mpcgomes2@gmail.com>
2024-09-20 18:10:23 -0300

    add local storage to i18n

--- app/i18n.ts
+++ app/i18n.ts
@@ -2,11 +2,14 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
+const savedLanguage = localStorage.getItem('language') || 'en';
+
 i18n
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
         fallbackLng: 'en',
+        lng: savedLanguage,
         resources: {
             en: {
                 translation: require('../public/locales/en-us/translation.json')
