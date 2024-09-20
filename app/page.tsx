4e20a27e580f75582e37cc984a493f88e8d1fc6e
Maria Júlia Miosso <mjuliamiosso@gmail.com>
2024-09-20 17:20:11 -0300

    add: process button

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import "./styles/page.scss";
 import Hero from './sections/Hero/Hero'
+import ProcessButton from './components/ProcessButton/ProcessButton'
 import ResourceCard from "./components/ResourceCard/ResourceCard";
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <main>
       <Hero></Hero>
+      <ProcessButton></ProcessButton>
     </main>
   );
 }
