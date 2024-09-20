afa3c943e81b11c958455cc4c76a669b83c15963
Marcos Gomes <mpcgomes2@gmail.com>
2024-09-20 17:47:32 -0300

    removed page unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 import "./styles/page.scss";
-import ResourceCard from "./components/ResourceCard/ResourceCard";
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 export default function Home() {
   return (
