89cf537a008b613443e8a68de191c07c030265c0
Maria Júlia Miosso <mjuliamiosso@gmail.com>
2024-09-23 16:51:13 -0300

    add: service card component

--- app/[locale]/home/page.tsx
+++ app/[locale]/home/page.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import './page.scss'
 import { useTranslation } from '../../hooks/useTranslation';
 import Hero from '../../sections/Hero/Hero'
+import ServiceCard from '../../components/ServiceCard/ServiceCard'
 
 const HomePage: React.FC = () => {
     const translations = useTranslation();
---
+++ app/components/ServiceCard/ServiceCard.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import styles from './ServiceCard.module.scss'
+import { FaCheck } from "react-icons/fa";
+
+interface ClientCardProps {
+    plan: string;
+    description: string;
+    price:number;
+    siteItem:string;
+    hostingItem:string;
+  }
+
+const ServiceCard: React.FC<ClientCardProps> = ({plan, description, price, siteItem, hostingItem}) => {
+  return (
+    <div className={styles['service-card-container']}>
+        <div className={styles['service-card-title']}>
+            <p className={styles['service-card-plan']}>{plan}</p>
+            <p className={styles['service-card-description']}>{description}</p>
+            <p className={styles['service-card-price']}>R$<span>{price}</span></p>
+            <button className={styles['service-card-btn']}>Escolher</button>
+        </div>
+        <div className={styles['service-card-list']}>
+            <div className={styles['service-card-site']}>
+                <p>Site</p>
+                <ul>
+                    <li><FaCheck />{siteItem}</li>
+                </ul>
+            </div>
+            <div className={styles['service-card-hosting']}>
+                <p>Hospedagem</p>
+                <ul>
+                    <li><FaCheck />{hostingItem}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ServiceCard
