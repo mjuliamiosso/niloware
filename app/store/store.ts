74ba39e5324995f2139993d07d09e694b1708818
Marcos Gomes <mpcgomes2@gmail.com>
2024-09-23 15:15:47 -0300

    remove language redux/store
    now use next's native i18n support

--- app/store/store.ts
+++ app/store/store.ts
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import themeReducer from './slices/themeSlice';
-import languageReducer from './slices/languageSlice';
 
 export const store = configureStore({
     reducer: {
         theme: themeReducer,
-        language: languageReducer
     }
 });
 
