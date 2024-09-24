78bf7ec2b5432ce69ed6889e20c602cfca6bf8f9
Maria Júlia Miosso <mjuliamiosso@gmail.com>
2024-09-24 17:14:29 -0300

    add: mobile hamburguer menu function

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,24 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react';
 import styles from './Navbar.module.scss'
 import classNames from 'classnames'
 import { IoMdMenu } from "react-icons/io";
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className={styles.navbar}>
+    <nav className={styles.navbar}>
         <div className={classNames(styles['navbar-container'], 'container')}>
             <h3>Nilo<span>ware</span></h3>
-            <div className={styles['navbar-links']}>
-                <a href="">Link</a>
-                <a href="">Link</a>
-                <a href="">Link</a>
+            <div className={classNames(styles['navbar-links'], { [styles.open]: isOpen })}>
+                <a href="">Recursos</a>
+                <a href="">Processos</a>
+                <a href="">Clientes</a>
+                <a href="">Serviço</a>
+                <a href="">Fale Conosco</a>
+                <a href="">FAQ</a>
             </div>
             <button className={styles['navbar-btn']}>Fale Conosco</button>
-            <button className={styles['navbar-menu']}>
+            <button className={styles['navbar-menu']} onClick={toggleMenu}>
                 <IoMdMenu />
             </button>
         </div>
-    </div>
+    </nav>
   )
 }
 
