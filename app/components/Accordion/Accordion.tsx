64c8964ee2bcbf51d47ff7c4918b0f7acc4b1984
Marcos Gomes <mpcgomes2@gmail.com>
2024-10-02 13:55:10 -0300

    refactor accordion

--- app/components/Accordion/Accordion.tsx
+++ app/components/Accordion/Accordion.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import styles from './Accordion.module.scss';
 import { LiaPlusSolid } from "react-icons/lia";
 import { LiaMinusSolid } from "react-icons/lia";
+import classNames from 'classnames';
 
 interface AccordionItem {
   title: string;
@@ -17,30 +18,41 @@ const Accordion: React.FC<AccordionProps> = ({ items }) => {
 
   const toggleAccordion = (index: number) => {
     setActiveIndex(activeIndex === index ? null : index);
-    
+
   };
 
   return (
     <div className={styles.accordion}>
-    {items.map((item, index) => (
-      <div key={index} className={styles['accordion-item']}>
-        <button
-          className={`${styles['accordion-button']} ${activeIndex === index ? styles.isOpen : ''}`}
-          onClick={() => toggleAccordion(index)}
-        >
-          {item.title}
-          <p>{activeIndex === index ? <LiaMinusSolid /> : <LiaPlusSolid />} </p>
-        </button>
+      {items.map((item, index) => (
         <div
-          className={`${styles['accordion-content']} ${
-            activeIndex === index ? styles.active : ''
-          }`}
+          key={index}
+          className={styles.accordionItem}
         >
-          <p>{item.content}</p>
+          <button
+            className={classNames(
+              styles.accordionButton,
+              { [styles.isOpen]: activeIndex === index }
+            )}
+            onClick={() => toggleAccordion(index)}
+          >
+            {item.title}
+            <p>
+              {activeIndex === index ? <LiaMinusSolid /> : <LiaPlusSolid />}
+            </p>
+          </button>
+          <div
+            className={classNames(
+              styles.accordionContent,
+              { [styles.active]: activeIndex === index }
+            )}
+          >
+            <p>
+              {item.content}
+            </p>
+          </div>
         </div>
-      </div>
-    ))}
-  </div>
+      ))}
+    </div>
   );
 };
 
