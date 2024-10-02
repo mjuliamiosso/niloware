1ff15699722189b64c594abbbd933b28826622e5
Marcos Gomes <mpcgomes2@gmail.com>
2024-10-02 15:35:18 -0300

    refactor footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -2,29 +2,40 @@ import React from 'react'
 import { FaFacebook, FaInstagram } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import styles from './Footer.module.scss'
-import classNames from 'classnames';
 
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <div className={classNames(styles['footer-container'], 'container')}>
-                <div className={styles['footer-logo']}>
-                    <h3>Niloware</h3>
-                    <div className={styles['footer-social']}>
-                        <a href=""><FaFacebook /></a>
-                        <a href=""><FaInstagram /></a>
-                        <a href=""><FaXTwitter /></a>
+            <div className={styles.container}>
+                <div className={styles.leftContainer}>
+                    <h3>
+                        Niloware
+                    </h3>
+                    <div className={styles.socials}>
+                        <a href="">
+                            <FaFacebook />
+                        </a>
+                        <a href="">
+                            <FaInstagram />
+                        </a>
+                        <a href="">
+                            <FaXTwitter />
+                        </a>
                     </div>
                 </div>
-                <div className={styles['footer-privacy']}>
-                    <div className={styles['footer-privacy-container']}>
-                        <a href="">Eula</a>
-                        <a href="">Aviso de Privacidade</a>
-                        <a href="">Termos de Uso</a>
-                        <a href="">Preferências de Cookies</a>
+                <div className={styles.rightContainer}>
+                    <div className={styles.links}>
+                        <a href="">
+                            Termos de Uso
+                        </a>
+                        <a href="">
+                            Políticas de Privacidade
+                        </a>
                     </div>
-                    <div className={styles['footer-copyright']}>
-                        <p>™ & © 2024 Niloware, Ltd. Todos os direitos reservados.</p>
+                    <div className={styles.copyright}>
+                        <p>
+                            ™ & © 2024 Niloware, Ltd. Todos os direitos reservados.
+                        </p>
                     </div>
                 </div>
             </div>
