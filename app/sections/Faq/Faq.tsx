3f42cb86de9199eb96a62e3485702b5ed799fa28
Marcos Gomes <mpcgomes2@gmail.com>
2024-10-04 18:01:15 -0300

    add custom accordion from mui material

--- app/sections/Faq/Faq.tsx
+++ app/sections/Faq/Faq.tsx
@@ -1,23 +1,25 @@
 import React from 'react'
 import styles from './Faq.module.scss'
-import classNames from 'classnames'
-import Accordion from '../../components/Accordion/Accordion';
+import CustomAccordion from '@/app/components/CustomAccordion/CustomAccordion';
 
 const Faq = () => {
   const items = [
     { title: 'Item 1', content: 'Descrição.' },
     { title: 'Item 2', content: 'Descrição.' },
-    { title: 'Item 3', content: 'Descrição.' }
+    { title: 'Item 3', content: 'Descrição.' },
+    { title: 'Item 4', content: 'Descrição.' },
+    { title: 'Item 5', content: 'Descrição.' },
+    { title: 'Item 6', content: 'Descrição.' }
   ];
 
   return (
     <section className={styles.faq}>
-      <div className={classNames(styles.container)}>
-        <h2 className={styles.subtitle}>
+      <div className={styles.container}>
+        <h2>
           Faq
         </h2>
         <div>
-          <Accordion items={items} />
+          <CustomAccordion items={items} />
         </div>
       </div>
     </section>
