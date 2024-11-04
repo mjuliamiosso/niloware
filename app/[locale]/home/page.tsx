bf1f4d8561c2942cc58b0b9fdea048b2fb2d4605
Maria Júlia Miosso <mjuliamiosso@gmail.com>
2024-11-04 19:43:29 -0300

    navbar ref

--- app/[locale]/home/page.tsx
+++ app/[locale]/home/page.tsx
@@ -17,6 +17,7 @@ const HomePage: React.FC = () => {
         <div>
             <Navbar
                 link={'Recursos'}
+                ref={''}
             />
             <Hero />
             <Resource />
